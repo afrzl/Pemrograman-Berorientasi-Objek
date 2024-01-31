57bf072ec6e53704e43cb3506139fd70f7ec1b94
Muh. Nur Afrizal <muhamadnurafrizal5@gmail.com>
2024-01-31 11:32:10 +0700

    pra week 3

--- Week 3/Pra Week/6/DiscountRate.java
+++ Week 3/Pra Week/6/DiscountRate.java
@@ -2,14 +2,14 @@
  * DiscountRate
  */
 public class DiscountRate {
-  static final double serviceDiscountPremium = 0.2;
-  static final double serviceDiscountGold = 0.15;
-  static final double serviceDiscountSilver = 0.1;
-  static final double productDiscountPremium = 0.1;
-  static final double productDiscountGold = 0.1;
-  static final double productDiscountSilver = 0.1;
+  private static final double serviceDiscountPremium = 0.2;
+  private static final double serviceDiscountGold = 0.15;
+  private static final double serviceDiscountSilver = 0.1;
+  private static final double productDiscountPremium = 0.1;
+  private static final double productDiscountSilver = 0.1;
+  private static final double productDiscountGold = 0.1;
 
-  static double getServiceDiscountRate(String type) {
+  public static double getServiceDiscountRate(String type) {
     switch (type) {
       case "Premium":
         return serviceDiscountPremium;
@@ -22,7 +22,7 @@ public class DiscountRate {
     }
   }
 
-  static double getProductDiscountRate(String type) {
+  public static double getProductDiscountRate(String type) {
     switch (type) {
       case "Premium":
         return productDiscountPremium;
